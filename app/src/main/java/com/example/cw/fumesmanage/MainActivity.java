4a6e14400d4b0bb1ecc8dc962c9c19c07490c7f6
xiaoshidefeng <1330661071@qq.com>
2017-03-22 20:28:03 +0800

    主界面框架完成 viewPager 实现

--- app/src/main/java/com/example/cw/fumesmanage/MainActivity.java
+++ app/src/main/java/com/example/cw/fumesmanage/MainActivity.java
@@ -1,9 +1,26 @@
 package com.example.cw.fumesmanage;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    //几个代表页面的常量
+    public static final int PAGE_ONE = 0;
+    public static final int PAGE_TWO = 1;
+    public static final int PAGE_THREE = 2;
+
+    private MyFragmentPagerAdapter mAdapter;
+
+    private RadioGroup rg_tab_bar;
+    private RadioButton rb_map;
+    private RadioButton rb_list;
+    private RadioButton rb_me;
+
+    private ViewPager vpager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +30,75 @@ public class MainActivity extends AppCompatActivity {
         SetBarColor setBarColor = new SetBarColor();
         setBarColor.MakeBarTrans(MainActivity.this);
 
+        mAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
 
-
+        initView();
+        rb_map.setChecked(true);
 
     }
+
+    private void initView() {
+
+            rg_tab_bar = (RadioGroup) findViewById(R.id.rg_tab_bar);
+            rb_map = (RadioButton) findViewById(R.id.rb_map);
+            rb_list = (RadioButton) findViewById(R.id.rb_list);
+            rb_me = (RadioButton) findViewById(R.id.rb_me);
+
+
+            rg_tab_bar.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                    switch (i) {
+                        case R.id.rb_map:
+                            vpager.setCurrentItem(PAGE_ONE);
+                            break;
+                        case R.id.rb_list:
+                            vpager.setCurrentItem(PAGE_TWO);
+                            break;
+                        case R.id.rb_me:
+                            vpager.setCurrentItem(PAGE_THREE);
+                            break;
+
+                    }
+                }
+            });
+
+
+            vpager = (ViewPager) findViewById(R.id.vpager);
+            vpager.setAdapter(mAdapter);
+            vpager.setCurrentItem(0);
+            vpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+                @Override
+                public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+                }
+
+                @Override
+                public void onPageSelected(int position) {
+
+                }
+
+                @Override
+                public void onPageScrollStateChanged(int state) {
+                    //state的状态有三个，0表示什么都没做，1正在滑动，2滑动完毕
+                    if (state == 2) {
+                        switch (vpager.getCurrentItem()) {
+                            case PAGE_ONE:
+                                rb_map.setChecked(true);
+                                break;
+                            case PAGE_TWO:
+                                rb_list.setChecked(true);
+                                break;
+                            case PAGE_THREE:
+                                rb_me.setChecked(true);
+                                break;
+
+                        }
+                    }
+
+                }
+            });
+    }
+
+
 }
---
+++ app/src/main/java/com/example/cw/fumesmanage/MyFragmentPagerAdapter.java
@@ -0,0 +1,61 @@
+package com.example.cw.fumesmanage;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.ViewGroup;
+
+/**
+ * Created by cw on 2017/3/22.
+ */
+
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    private final int PAGER_COUNT = 3;
+    private MapFragment myFragment1 = null;
+    private ListFragment myFragment2 = null;
+    private MeFragment myFragment3 = null;
+
+    public MyFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+        myFragment1 = new MapFragment();
+        myFragment2 = new ListFragment();
+        myFragment3 = new MeFragment();
+    }
+
+
+    @Override
+    public Object instantiateItem(ViewGroup vg, int position) {
+        return super.instantiateItem(vg, position);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        System.out.println("position Destory" + position);
+        super.destroyItem(container, position, object);
+    }
+
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        switch (position) {
+            case MainActivity.PAGE_ONE:
+                fragment = myFragment1;
+                break;
+            case MainActivity.PAGE_TWO:
+                fragment = myFragment2;
+                break;
+            case MainActivity.PAGE_THREE:
+                fragment = myFragment3;
+                break;
+
+        }
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGER_COUNT;
+    }
+}
